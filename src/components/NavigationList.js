06c2a3798de9a957a0e554cd052f6910fba22476
Peter Alserda <peteralserda@hotmail.com>
2016-04-07 13:22:06 +0200

    logic removed from return

--- src/components/NavigationList.js
+++ src/components/NavigationList.js
@@ -3,22 +3,41 @@ import React, { Component, PropTypes } from 'react';
 import NavigationItem from './NavigationItem'
 
 export default class NavigationList extends Component {
-  render() {
-    const { items, plusPath } = this.props
+  constructor() {
+    super()
+    this.state = {
+      navigationItems: [],
+    }
+  }
 
+  componentDidMount() {
+    this.returnNavigationItems()
+  }
+
+  returnNavigationItems() {
+    const { items, plusPath } = this.props
+    const retrievedNavigationItems = []
+
+    items.map((item, index) => {
+      switch (item) {
+        case 'plus':
+          retrievedNavigationItems.push(<NavigationItem item={item} key={index} path={plusPath} />)
+          return null
+        default:
+          return null
+      }
+    })
+
+    this.setState({
+      navigationItems: retrievedNavigationItems,
+    })
+  }
+
+  render() {
     return (
       <div className='navigationItems'>
         <ul>
-          {items.map((item, index) => {
-            switch (item) {
-              case 'plus':
-                return (
-                  <NavigationItem item={item} key={index} path={plusPath} />
-                )
-              default:
-                return null
-            }}
-          )}
+          {this.state.navigationItems}
         </ul>
       </div>
     )
